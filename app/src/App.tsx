0b754e8ea461d7d5d8096cef27e2efeb19c4ac12
LTH <johtta888@naver.com>
2023-01-29 14:29:27 +0900

    Feat: 추가 base url src

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+}
+
+export default App;
