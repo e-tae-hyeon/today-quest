e0d8f8e4a9ac23b83ee28d161e2b79c3c6a22345
LTH <johtta888@naver.com>
2023-01-30 00:29:15 +0900

    Feat: 추가 FadeContainer component

--- app/src/@shared/components/base/index.ts
+++ app/src/@shared/components/base/index.ts
@@ -2,6 +2,7 @@ export {default as AppText} from './AppText';
 export {default as Back} from './Back';
 export {default as BottomButton} from './BottomButton';
 export {default as Button} from './Button';
+export {default as FadeContainer} from './FadeContainer';
 export {default as FlexGapContainer} from './FlexGapContainer';
 export {default as Layout} from './Layout';
 export {default as SvgIcon} from './SvgIcon';
