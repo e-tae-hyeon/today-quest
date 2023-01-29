d0dcc9bf304517c0a930b92c462ef4e600ebc6ee
LTH <johtta888@naver.com>
2023-01-29 22:25:38 +0900

    Feat: 추가 SvgIcon component
    kakao-logo,mail svg icon 추가

---
+++ app/src/@shared/components/base/SvgIcon/icons.ts
@@ -0,0 +1,2 @@
+export {default as kakao} from 'assets/icons/kakao-logo.svg';
+export {default as mail} from 'assets/icons/mail.svg';
--- app/src/@shared/components/base/index.ts
+++ app/src/@shared/components/base/index.ts
@@ -3,3 +3,4 @@ export {default as BottomButton} from './BottomButton';
 export {default as Button} from './Button';
 export {default as FlexGapContainer} from './FlexGapContainer';
 export {default as Layout} from './Layout';
+export {default as SvgIcon} from './SvgIcon';
