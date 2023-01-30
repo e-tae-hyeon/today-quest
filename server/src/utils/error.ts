b772115ba6c3836afdb6694d3a0c7f2770e789e4
LTH <johtta888@naver.com>
2023-01-30 23:31:14 +0900

    Feat: 추가 error custom code
    커스텀 코드를 기반으로 에러 메시지를 보여줌

--- server/src/utils/error.ts
+++ server/src/utils/error.ts
@@ -1,15 +1,23 @@
 import { Context } from "koa";
 
 const errorsMap = {
+  Unknown: {
+    code: 0,
+    statusCode: 500,
+    message: "Unknown Error",
+  },
   NotFound: {
+    code: 1,
     statusCode: 404,
     message: "Not Found",
   },
   Invalid: {
+    code: 2,
     statusCode: 401,
     message: "Invalid",
   },
   Expired: {
+    code: 3,
     statusCode: 410,
     message: "Expired",
   },
@@ -21,19 +29,25 @@ export default class AppError extends Error {
   constructor(name: ErrorName) {
     super();
 
-    const { message, statusCode } = errorsMap[name];
-    this.message = message;
+    const { code, statusCode, message } = errorsMap[name];
+    this.code = code;
     this.statusCode = statusCode;
+    this.message = message;
   }
 
+  code: number;
   message: string;
   statusCode: number;
 }
 
 export function errorHandler(ctx: Context, err: AppError) {
-  const message = err.message ?? "Internal server error";
-  const status = err.statusCode ?? 500;
+  const code = err.code ?? errorsMap.Unknown.code;
+  const status = err.statusCode ?? errorsMap.Unknown.statusCode;
+  const message = err.message ?? errorsMap.Unknown.message;
 
   ctx.status = status;
-  ctx.body = message;
+  ctx.body = {
+    code,
+    message,
+  };
 }
