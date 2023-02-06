96858a7be1d4636d23fb1e265b33f705037a4e46
LTH <johtta888@naver.com>
2023-02-06 14:17:45 +0900

    Feat: 추가 MainTab Icons

--- app/src/@shared/components/base/SvgIcon/icons.ts
+++ app/src/@shared/components/base/SvgIcon/icons.ts
@@ -1,6 +1,9 @@
 export {default as checkCircle} from 'assets/icons/check-circle.svg';
 export {default as chevronLeft} from 'assets/icons/chevron-left.svg';
+export {default as home} from 'assets/icons/home.svg';
 export {default as infoCircle} from 'assets/icons/info-circle.svg';
 export {default as kakao} from 'assets/icons/kakao-logo.svg';
 export {default as mail} from 'assets/icons/mail.svg';
+export {default as quest} from 'assets/icons/quest.svg';
+export {default as user} from 'assets/icons/user.svg';
 export {default as xClose} from 'assets/icons/x-close.svg';
