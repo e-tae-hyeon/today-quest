d6f9cff254bbf53865de498f01613d8cc6fe207f
LTH <johtta888@naver.com>
2023-02-14 20:38:45 +0900

    Fix: 수정 오늘의 퀘스트 query key to todayQuest from todayQuests

--- app/src/features/today-quest/hooks/useTodayQuestQuery.ts
+++ app/src/features/today-quest/hooks/useTodayQuestQuery.ts
@@ -2,11 +2,11 @@ import {useQuery} from '@tanstack/react-query';
 import {getMyTodayQuests} from 'apis/me';
 
 function useTodayQuestQuery() {
-  const {data: todayQuests} = useQuery(['todayQuests'], getMyTodayQuests);
+  const {data: todayQuest} = useQuery(['todayQuest'], getMyTodayQuests);
 
-  const type = todayQuests?.type;
-  const quests = todayQuests?.payload ?? [];
-  const date = todayQuests?.date ?? '오늘 하루';
+  const type = todayQuest?.type;
+  const quests = todayQuest?.payload ?? [];
+  const date = todayQuest?.date ?? '오늘 하루';
 
   return {type, quests, date};
 }
