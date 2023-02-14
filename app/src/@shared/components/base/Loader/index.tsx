71d5c7f70404d8cefc2e774afe5be98c2f559066
LTH <johtta888@naver.com>
2023-02-14 23:51:23 +0900

    Design: 수정 Loader size, padding

--- app/src/@shared/components/base/Loader/index.tsx
+++ app/src/@shared/components/base/Loader/index.tsx
@@ -2,7 +2,7 @@ import {ActivityIndicator} from 'react-native';
 import React from 'react';
 
 function Loader() {
-  return <ActivityIndicator />;
+  return <ActivityIndicator size="large" className="p-4" />;
 }
 
 export default Loader;
